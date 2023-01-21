cef3a1654eda511c85317b107023ad4e4c916fd9
Miguel M <migge898@gmx.de>
2023-01-21 21:14:09 +0100

    Update test_basic.py

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,16 +1,4 @@
-# -*- coding: utf-8 -*-
 
-from .context import dasai
+def test_absolute_truth_and_meaning():
+    assert False
 
-import unittest
-
-
-class BasicTestSuite(unittest.TestCase):
-    """Basic test cases."""
-
-    def test_absolute_truth_and_meaning(self):
-        assert True
-
-
-if __name__ == '__main__':
-    unittest.main()
