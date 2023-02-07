5f92cc3c489efef3087068ac2f2c2b98a86d4f17
Lint Action <lint-action@samuelmeuli.com>
2023-02-07 23:48:50 +0000

    Fix code style issues with Black

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,23 +12,23 @@ from datetime import datetime
 
 sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
 
-project = 'dasai'
+project = "dasai"
 # copyright with generated year
 
 
-copyright = '%d, Miguel Mioskowski, Marie Koch, Damien Völker' % datetime.now().year
-author = 'Miguel Mioskowski, Marie Koch, Damien Völker'
-release = '0.1'
+copyright = "%d, Miguel Mioskowski, Marie Koch, Damien Völker" % datetime.now().year
+author = "Miguel Mioskowski, Marie Koch, Damien Völker"
+release = "0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx_copybutton',
+    "sphinx.ext.autodoc",
+    "sphinx_copybutton",
 ]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
 # enable edit on github with repo located at: https://github.com/migge898/DASC_STOCKMARKET_AI
@@ -43,8 +43,8 @@ html_context = {
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
-root_doc = 'index'
+root_doc = "index"
