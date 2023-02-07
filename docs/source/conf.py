ff57889dd13cdcc2d3ad30f53f028ad326b6b8a6
Miguel M <migge898@gmx.de>
2023-02-08 00:23:28 +0100

    uncommented theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,7 +42,7 @@ html_context = {
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 
 html_static_path = ['_static']
 
