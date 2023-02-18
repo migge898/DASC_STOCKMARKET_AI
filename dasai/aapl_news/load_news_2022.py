c270bc68b1d3359d7ea9e1dec0f8c9199556b904
Damien Völker <66248616+dev-damien@users.noreply.github.com>
2023-02-18 15:43:54 +0100

    module structure re-designe and some related bugfixes

--- dasai/aapl_news/load_news_2022.py
+++ dasai/aapl_news/load_news_2022.py
@@ -11,21 +11,11 @@ from dasai.helpers import get_raw_data_path
 load_dotenv()
 my_key = os.getenv("alphavantage_key")
 
-# generate date strings in a specific format
-# time_limits contains start-date and end-date for all months of 2022
-days_of_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-time_limits = []
-for i in range(1, 13):
-    month = f"{i}" if i > 9 else f"0{i}"  # two-digit month
-    time_limits.append(f"2022{month}01T0101")  # add start of month
-    time_limits.append(f"2022{month}{days_of_month[i - 1]}T2359")
-
 no_articles_msg = (
     "{'Information': 'No articles found. "
     "Please adjust the time range or refer to the API documentation "
     "https://www.alphavantage.co/documentation#newsapi and try again.'}"
 )
-
 limit_reached_msg = (
     "{'Note': 'Thank you for using Alpha Vantage! "
     "Our standard API call frequency is 5 calls per minute and 500 calls"
@@ -34,11 +24,28 @@ limit_reached_msg = (
 )
 
 
-def get_news_by_month(month: int):
+def generate_time_limit_strings_2022():
     """
-    help function to get apple news of 2022 by publishing month
+    Generate date strings in a specific format
+    :return: time_limits contains start-date and end-date for all months of 2022
+    """
+    days_of_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    time_limits = []
+    for i in range(1, 13):
+        month = f"{i}" if i > 9 else f"0{i}"  # two-digit month
+        time_limits.append(f"2022{month}01T0101")  # add start of month
+        time_limits.append(f"2022{month}{days_of_month[i - 1]}T2359")
+    return time_limits
+
+
+def get_news_by_month(month: int, time_limits: []):
+    """
+    Helper function to get the 200 most relevant apple news of 2022 by publishing month
+    :param time_limits: time limits for all months of the specific year
+    :type time_limits: []
     :param month: the month of the news you want
-    :return: news of specified month
+    :type month: int
+    :return: 200 most relevant apple news of specified month
     """
     assert month >= 1 & month <= 12
     tickers = "AAPL"
@@ -66,45 +73,82 @@ def get_news_by_month(month: int):
     return True, pd.DataFrame(data["feed"])
 
 
-# get the news for each month and concatenate
-retry_counter = 0
-max_retries = 3
+def get_aapl_news_2022():
+    """
+    Gets the apple news for each month of 2022 and concatenate to one Dataframe
+    :return: The concatenated Dataframe with
+    """
+    retry_counter = 0
+    max_retries = 3
+    time_limits = generate_time_limit_strings_2022()
 
-df = pd.DataFrame()
-i = 1
-while i <= 12 and retry_counter < max_retries:
-    try:
-        is_data_found, df_month = get_news_by_month(i)
-        if is_data_found:
-            # reset retry counter
-            print(f"Month {i}: Data has been successfully fetched")
-            retry_counter = 0
-            df = pd.concat([df, df_month])
-        else:
-            print(f"Month {i}: No data has been found")
-        i += 1
-    except Exception as e:
-        if str(e) == "API token limit exceeded":
-            retry_counter += 1
-            print(
-                f"Month {i}: API call limit reached. "
-                f"Waiting for 65 seconds before retrying "
-                f"(retry={retry_counter}) ..."
-            )
-            time.sleep(65)
-        else:
-            print(f"Month {i}: Exception occurred. Month will be skipped")
+    df = pd.DataFrame()
+    i = 1
+    while i <= 12 and retry_counter < max_retries:
+        try:
+            is_data_found, df_month = get_news_by_month(i, time_limits)
+            if is_data_found:
+                # reset retry counter
+                print(f"Month {i}: Data has been successfully fetched")
+                retry_counter = 0
+                df = pd.concat([df, df_month])
+            else:
+                print(f"Month {i}: No data has been found")
             i += 1
+        except Exception as e:
+            if str(e) == "API token limit exceeded":
+                retry_counter += 1
+                print(
+                    f"Month {i}: API call limit reached. "
+                    f"Waiting for 65 seconds before retrying "
+                    f"(retry={retry_counter}) ..."
+                )
+                time.sleep(65)
+            else:
+                print(f"Month {i}: Exception occurred. Month will be skipped")
+                i += 1
+    return df
 
 
-# reset index for unique indexes
-df = df.reset_index()
+def adjust_dataframe(df: pd.DataFrame):
+    """
+    This function cleans leftovers created by the concatenations
+    :param df: Dataframe to adjust
+    :type df: pandas.Dataframe
+    :return: Adjusted Dataframe
+    """
+    # reset index for unique indexes
+    df = df.reset_index()
 
-# drop index column inherited from monthly dataframes
-df = df.drop(["index"], axis=1)
+    # drop index column inherited from monthly dataframes
+    df = df.drop(["index"], axis=1)
 
-# safe the json object to a file for easier access
-raw_data_path = get_raw_data_path()
-raw_data_path.mkdir(parents=True, exist_ok=True)
+    return df
 
-df.to_json(raw_data_path / "aapl_news.json")
+
+def save_dataframe_to_file(df: pd.DataFrame):
+    """
+    Save the Dataframe to a json file
+    :param df: Dataframe to save
+    :type df: pandas.Dataframe
+    :return: None
+    """
+    # safe the json object to a file for easier access
+    raw_data_path = get_raw_data_path()
+    raw_data_path.mkdir(parents=True, exist_ok=True)
+
+    df.to_json(raw_data_path / "aapl_news.json")
+
+
+def load_and_save_news_2022():
+    """
+    This function will get the news, adjust them and then save them to a .json file
+    :return:
+    """
+    df = get_aapl_news_2022()
+    df = adjust_dataframe(df)
+    save_dataframe_to_file(df)
+
+
+if __name__ == '__main__':
+    load_and_save_news_2022()
--- dasai/aapl_news/tidy_data.py
+++ dasai/aapl_news/tidy_data.py
@@ -3,23 +3,43 @@ import pandas as pd
 from dasai.helpers import get_raw_data_path
 from dasai.helpers import get_tidy_data_path
 
-# load data from json file
-raw_data_path = get_raw_data_path()
-df = pd.read_json(raw_data_path / "aapl_news.json")
 
-# Transform the custom date-format-string to a real date
-df["time_published"] = pd.to_datetime(df["time_published"], format="%Y%m%dT%H%M%S")
+def load_apple_news():
+    """
+    Loads the apple news from the .json file
+    :return: Dataframe with raw news
+    """
+    raw_data_path = get_raw_data_path()
+    return pd.read_json(raw_data_path / "aapl_news.json")
 
-# Keep only interesting columns
-df = df[
-    [
-        "time_published",
-        "source",
-        "category_within_source",
-        "overall_sentiment_score",
-        "ticker_sentiment",
+
+def transform_date(df: pd.DataFrame):
+    """
+    Transform the custom date-format-string to a real datetime
+    :param df: Dataframe for which the column should be transformed
+    :type df: pandas.Dataframe
+    :return: Dataframe with transformed datetime column
+    """
+    df["time_published"] = pd.to_datetime(df["time_published"], format="%Y%m%dT%H%M%S")
+    return df
+
+
+def remove_useless_columns(df: pd.DataFrame):
+    """
+    Keep only relevant columns for analysis
+    :param df: Dataframe to remove columns from
+    :type df: pandas.Dataframe
+    :return: reduced Dataframe
+    """
+    return df[
+        [
+            "time_published",
+            "source",
+            "category_within_source",
+            "overall_sentiment_score",
+            "ticker_sentiment",
+        ]
     ]
-]
 
 
 # Remove all ticker details despite the relevant AAPL one
@@ -30,6 +50,7 @@ def flatten_nested_json_df(df):
     """
     Search for columns to explode/flatten recursively
     :param df: Dataframe to flatten
+    :type df: pandas.Dataframe
     :return: Flattened and exploded dataframe
     """
     df = df.reset_index()
@@ -76,34 +97,78 @@ def flatten_nested_json_df(df):
     return df
 
 
-# first flatten the json list ticker_sentiment
-df = flatten_nested_json_df(df)
-df = df.set_index("index")
+def flatten_ticket_sentiment(df: pd.DataFrame):
+    """
+    Flatten/explode the json list ticker_sentiment
+    :param df: Dataframe to flatten
+    :type df: pandas.Dataframe
+    :return: Flat and exploded Dataframe
+    """
+    df = flatten_nested_json_df(df)
+    df = df.set_index("index")
 
-# then keep only the entries with the relevant ticker
-df = df.query('`ticker_sentiment.ticker` == "AAPL"')
+    return df
 
-# remove ticker column (it contains "AAPL" for every entry)
-df = df.drop(
-    ["ticker_sentiment.ticker", "ticker_sentiment.ticker_sentiment_label"], axis=1
-)
 
-# rename columns
-df = df.rename(
-    columns={
-        "category_within_source": "category",
-        "ticker_sentiment.relevance_score": "relevance_score_aapl",
-        "ticker_sentiment.ticker_sentiment_score": "sentiment_score_aapl",
-    }
-)
+def tidy_data(df: pd.DataFrame):
+    """
+    A function to tidy the raw apple news data up
+    (keep only relevant ticker, remove useless column 'ticker',
+    rename some columns and set correct datatypes)
+    :param df: Dataframe to clean
+    :type df: pandas.Dataframe
+    :return: A clean and reduced version of the input Dataframe
+    """
+    # keep only the entries with the relevant ticker
+    df = df.query('`ticker_sentiment.ticker` == "AAPL"')
 
-# set correct datatypes
-df["category"] = df["category"].astype("category")
-df["relevance_score_aapl"] = df["relevance_score_aapl"].astype("float64")
-df["sentiment_score_aapl"] = df["sentiment_score_aapl"].astype("float64")
+    # remove ticker column (it contains "AAPL" for every entry)
+    df = df.drop(
+        ["ticker_sentiment.ticker", "ticker_sentiment.ticker_sentiment_label"], axis=1
+    )
 
-# save tidy data in better format
-tidy_data_path = get_tidy_data_path()
-tidy_data_path.mkdir(parents=True, exist_ok=True)
-df.to_parquet(tidy_data_path / "aapl_news.parquet")
-print(f"Dataframe has been saved at {tidy_data_path / 'aapl_news.parquet'}")
+    # rename columns
+    df = df.rename(
+        columns={
+            "category_within_source": "category",
+            "ticker_sentiment.relevance_score": "relevance_score_aapl",
+            "ticker_sentiment.ticker_sentiment_score": "sentiment_score_aapl",
+        }
+    )
+
+    # set correct datatypes
+    df["category"] = df["category"].astype("category")
+    df["relevance_score_aapl"] = df["relevance_score_aapl"].astype("float64")
+    df["sentiment_score_aapl"] = df["sentiment_score_aapl"].astype("float64")
+
+    return df
+
+
+def save_dataframe(df: pd.DataFrame):
+    """
+    Saves the specified Dataframe as a .parquet file
+    :param df: The Dataframe to save
+    :type df: pandas.Dataframe
+    :return: None
+    """
+    tidy_data_path = get_tidy_data_path()
+    tidy_data_path.mkdir(parents=True, exist_ok=True)
+    df.to_parquet(tidy_data_path / "aapl_news.parquet")
+    print(f"Dataframe has been saved at {tidy_data_path / 'aapl_news.parquet'}")
+
+
+def tidy_and_save_news():
+    """
+    Loads the raw .json file, create tidy data from it and save this as a .parquet file
+    :return: None
+    """
+    df = load_apple_news()
+    df = transform_date(df)
+    df = remove_useless_columns(df)
+    df = flatten_ticket_sentiment(df)
+    df = tidy_data(df)
+    save_dataframe(df)
+
+
+if __name__ == '__main__':
+    tidy_and_save_news()
