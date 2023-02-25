e0b51174d0748681d60d55051894bae48e3603dc
Lint Action <lint-action@samuelmeuli.com>
2023-02-25 16:45:46 +0000

    Fix code style issues with Black

--- tests/test_aapl_news.py
+++ tests/test_aapl_news.py
@@ -12,6 +12,18 @@ def test_df_shape():
 def test_columns():
     raw_data_path = get_raw_data_path()
     df = pd.read_json(raw_data_path / "aapl_news.json")
-    assert set(df.columns) == {'title', 'url', 'time_published', 'authors', 'summary', 'banner_image', 'source',
-                               'category_within_source', 'source_domain', 'topics', 'overall_sentiment_score',
-                               'overall_sentiment_label', 'ticker_sentiment'}
+    assert set(df.columns) == {
+        "title",
+        "url",
+        "time_published",
+        "authors",
+        "summary",
+        "banner_image",
+        "source",
+        "category_within_source",
+        "source_domain",
+        "topics",
+        "overall_sentiment_score",
+        "overall_sentiment_label",
+        "ticker_sentiment",
+    }
