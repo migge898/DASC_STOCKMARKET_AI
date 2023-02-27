4ed9415aab49108ade8839520e3f61d5339b9b9b
mariik0 <marie.koch@stud.th-bingen.de>
2023-02-27 01:41:05 +0100

    outsourced news_dashboard, fixed stock selection

--- dasai/dashboard/dashboard_news.py
+++ dasai/dashboard/dashboard_news.py
@@ -20,6 +20,8 @@ symbol = symbols[0]
 stock_names = {'AAPL': 'Apple', 'NFLX': 'Netflix', 'KO': 'Coca Cola', 'IBM': 'IBM', 'MSFT': 'Microsoft'}
 
 df_stocks = pd.read_parquet('..\..' / cleaned_data_path / f'{symbol}.parquet')
+if(symbol == 'AAPL'):
+    df_news = pd.read_parquet('..\..' / tidy_data_path / f'{symbol.lower()}_news_dense.parquet')
 df_result = pd.read_csv('..\..' / result_data_path / f'{symbol}_prediction.csv')
 
 #print(df_result)
@@ -111,18 +113,18 @@ figw.update_layout(
     yaxis_title='predicted values',
     title='Weekly'
 )
+# Define the dropdown options
+options = [{'label': i, 'value': i} for i in df_news['source'].unique()]
 
-figy = px.line(x=df_result['ds'],
-                y=df_result['yearly']
-               )
-
-
-
-# Set the axis labels
-figy.update_layout(
-    xaxis_title='date',
-    yaxis_title='predicted values',
-    title='Yearly'
+fig2 = px.scatter(
+    df_news,
+    x='time_published',
+    y=f'sentiment_score_{symbol.lower()}',
+    color='source',
+    size=f'relevance_score_{symbol.lower()}',
+    color_continuous_scale='ice',
+    opacity=0.6,
+    labels={'x': 'date', 'y': 'sentiment score'}
 )
 
 app.layout = html.Div(children=[
@@ -132,10 +134,8 @@ app.layout = html.Div(children=[
         options=symbol_options,
         value=symbols[0]
     ),
-    html.Div(
-        id='stock_header'
-    ),
-    #html.H1(f'{stock_names.get(symbol)} Stocks'),
+
+    html.H1(f'{stock_names.get(symbol)} Stocks'),
 
     html.Div([
         html.H4('Stock History'),
@@ -153,107 +153,61 @@ app.layout = html.Div(children=[
         dcc.Graph(
             id='weekly_prediction',
             figure=figw
-        ),
-        dcc.Graph(
-            id='yearly_prediction',
-            figure=figy
         )
     ]),
 
-
+    html.Div([
+        html.H4('Sentiment Score by News'),
+        html.P('News Source:'),
+        dcc.Dropdown(id='source-dropdown', options=options, value='Forbes'),
+        dcc.Graph(
+            id='news_graph',
+            figure=fig2
+        )
+    ])
 ])
 
 
 # Define the callback to update the graph based on the dropdown selection
 
-# Define the callback to update the graph based on the dropdown selection
 @app.callback(
     [
         Output('stock_graph', 'figure'),
-        Output('stock_graph_prediction', 'figure'),
-        Output('weekly_prediction', 'figure'),
-        Output('yearly_prediction', 'figure'),
-        [Output('stock_header', 'children')]
+        Output('news_graph', 'figure')
     ],
     [
-        Input('symbol-dropdown', 'value')
+        Input('symbol-dropdown', 'value'),
+        Input('source-dropdown', 'value')
     ]
-)
-def update_graph(selected_symbol):
-    df_selected = pd.read_parquet('..\..' / cleaned_data_path / f'{selected_symbol}.parquet')
 
+)
+def update_graph(symbol, selected_news_source):
+    df_stocks = pd.read_parquet('..\..' / cleaned_data_path / f'{symbol}.parquet')
+    if (symbol == 'AAPL'):
+
+        df_news = pd.read_parquet('..\..' / tidy_data_path / f'{symbol.lower()}_news_dense.parquet')
+
+        filtered_df = df_news[df_news['source'] == selected_news_source]
+
+        #options = [{'label': i, 'value': i} for i in df_news['source'].unique()]
+
+        fig_news = px.scatter(
+            filtered_df,
+            x='time_published',
+            y=f'sentiment_score_{symbol.lower()}',
+            color='source',
+            size=f'relevance_score_{symbol.lower()}',
+            opacity=0.6
+        )
     fig_stocks = px.line(
-        df_selected,
-        x=df_selected.index,
+        df_stocks,
+        x=df_stocks.index,
         y='adjusted_close',
         labels={'index': 'date', 'adjusted_close': 'adjusted close values'}
     )
-
-    df_result = pd.read_csv('..\..' / result_data_path / f'{selected_symbol}_prediction.csv')
-    fig1 = go.Figure()
-    fig1.add_trace(
-        go.Scatter(
-            x=df_result['ds'],
-            y=df_result['yhat'],
-            name='predicted value'
-        )
-    )
-    actual = df_selected.loc[df_selected.index.isin(df_result['ds'])]
-    fig1.add_trace(
-        go.Scatter(
-            x=df_result['ds'],
-            y=actual['adjusted_close'],
-            name='actual adjusted close value',
-            line_color='indigo'
-        )
-    )
-    fig1.add_trace(
-        go.Scatter(
-            x=df_result['ds'],
-            y=df_result['yhat_upper'],
-            fill=None,
-            mode='lines',
-            line_color='rgba(0,0,0,0)',
-            showlegend=False
-        )
-    )
-    fig1.add_trace(
-        go.Scatter(
-            x=df_result['ds'],
-            y=df_result['yhat_lower'],
-            fill='tonexty',
-            mode='lines',
-            line_color='rgba(0,0,0,0)',
-            name='confidence interval'
-        )
-    )
-    fig1.update_layout(
-        xaxis_title='date',
-        yaxis_title='predicted weekly values',
-        title='Prediction'
-    )
-
-    figw = px.line(
-        x=df_result['ds'],
-        y=df_result['weekly']
-    )
-    figw.update_layout(
-        xaxis_title='date',
-        yaxis_title='predicted values',
-        title='Weekly'
-    )
-
-    figy = px.line(x=df_result['ds'],
-                   y=df_result['yearly']
-                   )
-
-    figy.update_layout(
-        xaxis_title='date',
-        yaxis_title='predicted values',
-        title='Yearly'
-    )
-
-    return fig_stocks, fig1, figw, figy,  [html.H1(f'{stock_names.get(selected_symbol)} Stocks')]
+    if(symbol == 'AAPL'):
+        return fig_stocks, fig_news
+    return fig_stocks
 
 if __name__ == '__main__':
     app.run_server(debug=True)
